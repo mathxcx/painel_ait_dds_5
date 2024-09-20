0067c2ef54c8879ff5f3a8f213bf65f09ed90ca2
MATHEUS SARTI BRUNELLI <m.brunelli@labcetec.net>
2024-09-20 17:00:25 -0300

    Adicionando imagens e estilizando home

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import styles from './Home.module.css'
 import Cabecalho from '../layout/Cabecalho';
 import TabelaAulas from '../tabelaAulas/TabelaAulas';
+import LateralImagens from '../LateralImagens/LateralImagens';
 
 function Home() {
   return (
     <>
     <Cabecalho/>
     <TabelaAulas/>
+    <LateralImagens/>
     </>
     
   )
--- client/src/components/tabelaAulas/TabelaAulas.jsx
+++ client/src/components/tabelaAulas/TabelaAulas.jsx
@@ -3,6 +3,7 @@ import AbreviaData from "./AbreviaData";
 import AbreviaAmbiente from "./AbreviaAmbiente";
 import AbreviaInstrutor from "./AbreviaInstrutor";
 import AbreviaUC from "./AbreviaUC";
+import styles from './TabelaAulas.module.css';
 
 function TabelaAulas() {
     const [aulas,setAulas] = useState([]);
@@ -33,8 +34,8 @@ function TabelaAulas() {
 
   }
   return (
-    <div>
-        <table>
+    <div className={styles.aulas}>
+        <table className={styles.tabelaAulas}>
             <thead>
                 <tr>
                 <th>Início</th>
